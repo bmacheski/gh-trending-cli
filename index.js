4aaf0090c5537e8be5595fb0122115272fcf0f59
bmacheski <bwm0007@gmail.com>
2016-02-15 18:17:32 -0600

    Init

---
+++ index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const yargs  = require('yargs')
+  , repos    = require('./lib/repos')
+  , devs     = require('./lib/devs')
+  ;
+
+const argv  = yargs.argv
+  , args    = argv._
+  ;
+
+if (args.length > 0) {
+  let arg = args.toString().toLowerCase();
+  if (arg == 'repos' || arg === 'repositories') {
+   repos();
+  } else if (arg == 'devs' || arg === 'developers') {
+    devs();
+  } else {
+    console.error('Not a valid argument.');
+    process.exit(1);
+  }
+}
