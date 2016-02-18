62ce114b9d84f45fa2449b581cb07be79c5643e5
bmacheski <bwm0007@gmail.com>
2016-02-18 09:19:55 -0600

    Create help message

--- index.js
+++ index.js
@@ -5,14 +5,19 @@
 const yargs = require('yargs')
     , repos = require('./lib/repos')
     , devs  = require('./lib/devs')
-
-const argv = yargs.argv
-    , args = argv._
+    , help  = require('./lib/help')
     ;
+
+const argv    = yargs.argv
+    , args    = argv._
+    , helpArg = argv.help
+    ;
+
 
 if (args.length > 0) {
   let arg = args.toString().toLowerCase();
   let lang = argv.lang;
+
   if (arg === 'repos' || arg === 'repositories' && lang) {
     repos.getReposByLang(lang);
   }
@@ -25,4 +30,10 @@ if (args.length > 0) {
     console.error('Not a valid argument.');
     process.exit(1);
   }
+}
+else if (helpArg) {
+  help.showHelp();
+}
+else {
+  console.error('Not a valid command. Type --help to view available commands.');
 }
